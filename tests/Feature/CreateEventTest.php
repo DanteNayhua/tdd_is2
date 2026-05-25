32eb954c85ea083eefcb8e2a56a78ea299a24d04
Danfast <2021009441c@utea.edu.pe>
2026-05-25 13:36:51 -0500

    test

--- tests/Feature/CreateEventTest.php
+++ tests/Feature/CreateEventTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Carbon\Carbon;
 
 class CreateEventTest extends TestCase
 {
